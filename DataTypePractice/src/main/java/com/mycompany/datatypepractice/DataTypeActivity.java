370d4966e96f811b9695dd8dfa63eb6b947caa78
leletu-kamana <st10514888@rcconnect.edu.za>
2026-09-02 20:55:56 +0200

    Add Java data type practice project
    Creates a new Maven-based Java project for basic datatype practice. This commit adds the project scaffold, a simple Hello World entry point, and a DataTypeActivity class that demonstrates integer, float, character, boolean, and String variables with output to the console.

---
+++ DataTypePractice/src/main/java/com/mycompany/datatypepractice/DataTypeActivity.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.datatypepractice;
+
+/**
+ *
+ * @author Lelethu Kamana
+ */
+public class DataTypeActivity {
+    public static void main(String[] args) {
+        //Declaring Integer Variable
+        int myAge = 19;
+        //Output
+        System.out.println("I am " + myAge + " years old");
+        
+        //Declaring Float Variable
+        float myDecimalNum = 41.99f;
+        //Output 
+        System.out.println("My favourite decimal number is " + myDecimalNum);
+        
+        //Declaring Character Variable
+        char myInitial = 'L';
+        //Output
+        System.out.println("My first Initial is " + myInitial);
+        
+        //Declaring Boolean Variable
+        boolean likeJava = true;
+        boolean dislikeJava = false;
+        //Output
+        System.out.println("Do I really like Java: " + likeJava);
+        System.out.println("Will I ever dislike Java: " + dislikeJava);
+        
+        //Declaring String Variable 
+        String myName = "Leletu ";
+        String mySurname = "Kamana";
+        //Output
+        System.out.println("My full name is " + myName + mySurname);
+    }
+}
